1b53af48abb804596bbd934640ffb1ad6fbaff86
joekroese <joekroese@gmail.com>
2021-12-04 16:48:00 +0100

    document datasets

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' Example Census
+#'
+#' A simulated dataset for use as an example census
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{region}{Regions of a country represented as letters}
+#'   \item{age_group}{Age group}
+#'   \item{ethnicity}{Ethnicity}
+#'   \item{gender}{Gender}
+#'   \item{n}{Number of people in this category in the country}
+#'   ...
+#' }
+#' @source Simulated in package
+"example_census"
+
+#' Example Survey
+#'
+#' A simulated dataset for use as an example survey
+#'
+#' @format A data frame
+#' \describe{
+#'   \item{region}{Regions of a country represented as letters}
+#'   \item{age}{Age in years}
+#'   \item{ethnicity}{Ethnicity}
+#'   \item{non_negative_response}{A non-negative response to a question e.g. the amount of money they make}
+#'   \item{binary_response}{A binary response to a question e.g. if they play golf}
+#'   ...
+#' }
+#' @source Simulated in package
+"example_survey"

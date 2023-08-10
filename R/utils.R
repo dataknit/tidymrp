0be836e8360d86b4afd70a62941a9a63118b3f4c
Jo Kroese <jo@jokroese.com>
2023-08-10 19:09:39 +0100

    remove comment

--- R/utils.R
+++ R/utils.R
@@ -12,7 +12,5 @@ get_independent_variables <- function(model) {
   all_variables <- names(model$data)
   independent_variables <- all_variables |>
     purrr::keep(function(x) !stringr::str_detect(x, dependent_variable))
-  # independent_variables |>
-    # .[!. %in% grep("[:*]", ., value = TRUE)] # remove interaction terms in formula
   return(independent_variables)
 }
